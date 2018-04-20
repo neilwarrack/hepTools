400727c0f6f1e8de419a463e7e85536b268e180f
Neil Warrack <neil.warrack@gmail.com>
2018-04-20 18:55:17 +0100

    tidy up file - remove comments etc

--- root/compRoot.cxx
+++ root/compRoot.cxx
@@ -19,27 +19,6 @@
   Designed to confirm that changes made to the ATLAS trigger code do not effect the code functionality
 */
 
-// ============= Neil's Debug Toolkit ============== //                                                                         
-
-unsigned int olYeller = 1;
- 
-#define N1 do { std::cout << "1 "; } while(0)
-#define N2 do { std::cout << "2 "; } while(0)
-#define N3 do { std::cout << "3 "; } while(0)
-#define N4 do { std::cout << "4 "; } while(0)
-#define N5 do { std::cout << "5 "; } while(0)
-#define N6 do { std::cout << "6 "; } while(0)
-#define N7 do { std::cout << "7 "; } while(0)
-#define N8 do { std::cout << "8 "; } while(0)
-#define N9 do { std::cout << "9 "; } while(0)
-#define WORRY   do { std::cout << "WORRY!"   << endl; } while(0)
-#define SUCCESS do { std::cout << "SUCCESS!" << endl; } while(0)
-#define BEGIN   do { std::cout << "Begin..." << endl; } while(0)
-#define END     do { std::cout << "End..."   << endl; } while(0)
-#define YELLN   do { std::cout << " -> Error-" << olYeller << endl; olYeller++; } while(0)
-#define YELL    do { std::cout << " -> Error" << endl; } while(0)
-#define RET     do { std::cout << endl; } while(0)
-// =================================================== //        
 
 using namespace std;
 
@@ -55,7 +34,7 @@ int compTProfile(TDirectoryFile* f1, TDirectoryFile* f2, TKey* key);
 
 unsigned int filenum=14;
 
-void compare_recursivefunctions()
+void compRoot()
 {
 
   TDirectory *where = gDirectory; //gDirectory = global directory         
@@ -79,74 +58,23 @@ void compare_recursivefunctions()
   TFile *file2 = new TFile(fileStr2);
 
   int *errorCtr = new int(0);
-  //int *errorCtr;
-  //*errorCtr = 2;
-  YELL;
-  cout << errorCtr << endl;  
-  YELL;
- *errorCtr = compareContents(file1, file2, errorCtr);      
 
-  // define variables
-  /*
-  TKey *key1, *key2;
-  //  TObject *obj1, *obj2;
-  string dirStr1, dirStr2;
+  *errorCtr = compareContents(file1, file2, errorCtr);      
 
-  
-  //search first file
-  TIter dirItr1( file1->GetListOfKeys());
-  TIter dirItr2( file2->GetListOfKeys());
-
-  while ((key1 = (TKey *) dirItr1())) { // while directory 1 still has sub directories 
-    key2 = (TKey *) dirItr2(); // iterate directory 2
-    //    YELLN; 
-    // get and compare directory names
-    dirStr1 = key1->GetName();
-    dirStr2 = key2->GetName();
-    if (dirStr1 != dirStr2) {
-      cout << "ABORT: Mismatch of top-level directory names! " << endl;
-      cout << "Root file must have same directory layout." << endl;
-      cout << "NB: "<< dirStr1 << " != " << dirStr2 << endl; 
-    } else {
-
-
-      // Get directory
-      TDirectoryFile* dir1=(TDirectoryFile*)file1->Get(key1->GetName());  
-      TDirectoryFile* dir2=(TDirectoryFile*)file2->Get(key2->GetName());  
-      ///////////////////////////////////////////////////////////////////////////
-    
-      //      errorCtr = searchDirectory(dir1, dir2);      
-     
-
-    } // end of matching names case
-    
-  }// end of while loop
-  */
-
-
-  
-  // final information output for user:
- cout << " - - - - - - FINISHED - - - - - - - " << endl << endl;
+  cout << " - - - - - - FINISHED - - - - - - - " << endl << endl;
  
- if (*errorCtr){
-   printf(" ERROR: No. of Mismatches: %d\n", *errorCtr);
- } else {
-      cout << "SUCCESS! No mismatches in any files" << endl;
-
-     }
- cout << "NB: Only TH1I, TH1F, TH2I, TH2F and TProfile files were checked." << endl;
+  if (*errorCtr){
+    printf(" ERROR: No. of Mismatches: %d\n", *errorCtr);
+  } else {
+    cout << "SUCCESS! No mismatches in any files" << endl;
+    
+  }
+  cout << "NB: Only TH1I, TH1F, TH2I, TH2F and TProfile files were checked." << endl;
 }
 
 
 
 int compareContents(TDirectoryFile* directory1, TDirectoryFile* directory2, int* mCtr){
-
-  // define variables
-  // int i=0, j=0, errorCtr=0;
-  // TKey *key1, *key2, *subKey1, *subKey2;
-  // TObject *obj1, *obj2;
-  // string dirStr1, dirStr2, subDirStr1, subDirStr2, subSubDirStr1, subSubDirStr2, classname ;
-  
 
   TKey *fileKey1, *fileKey2;
   string name1, name2, subDirectory1ClassName;
@@ -157,7 +85,7 @@ int compareContents(TDirectoryFile* directory1, TDirectoryFile* directory2, int*
 
   while ((fileKey1 = (TKey *) iterator1())) { // while directory 1 still has sub directories 
     fileKey2 = (TKey *) iterator2(); // iterate directory 2
-    //    YELLN; 
+   
     // get and compare directory names
     name1 = fileKey1->GetName();
     name2 = fileKey2->GetName();
@@ -171,8 +99,7 @@ int compareContents(TDirectoryFile* directory1, TDirectoryFile* directory2, int*
       // Get directory
       TDirectoryFile* subDirectory1=(TDirectoryFile*)directory1->Get(fileKey1->GetName());  
       TDirectoryFile* subDirectory2=(TDirectoryFile*)directory2->Get(fileKey2->GetName());  
-      //cout << ->ClassName() << endl;
-      //
+   
       
       subDirectory1ClassName = subDirectory1->ClassName();
       if (subDirectory1ClassName == "TDirectoryFile"){
@@ -196,14 +123,12 @@ int compareContents(TDirectoryFile* directory1, TDirectoryFile* directory2, int*
 
 
 int compTH1F(TDirectoryFile* f1, TDirectoryFile* f2, TKey* key){
-  TH1F * h1 = (TH1F*)f1;//->Get(key->GetName());
-  TH1F * h2 = (TH1F*)f2;//->Get(key->GetName());
-
-  int totBins = h1->GetSize();
- 
+  TH1F * h1 = (TH1F*)f1;
+  TH1F * h2 = (TH1F*)f2;
   double bin1=0, bin2=0, binDif=0, difTot=0;
   int difCtr=0;
-  
+  int totBins = h1->GetSize(); 
+
   for (int i=0; i<totBins; i++ ){
     bin1 = h1->GetBinContent(i);
     bin2 = h1->GetBinContent(i);
@@ -214,8 +139,7 @@ int compTH1F(TDirectoryFile* f1, TDirectoryFile* f2, TKey* key){
     }
   }
   
- 
-  // user output 
+   // user output 
   if (difTot){
     cout << "FAIL! (difference in " << difCtr << "/" << totBins << " bins)" << endl;
     return 1;
@@ -228,15 +152,11 @@ int compTH1F(TDirectoryFile* f1, TDirectoryFile* f2, TKey* key){
 
 
 int compTH1I(TDirectoryFile* f1, TDirectoryFile* f2, TKey* key){
-
   TH1I * h1 = (TH1I*)f1;
   TH1I * h2 = (TH1I*)f2;
-
-
-  int totBins = h1->GetSize();
-
   int bin1=0, bin2=0, binDif=0, difCtr=0, difTot=0;
-  
+  int totBins = h1->GetSize();  
+
   for (int i=0; i<totBins; i++ ){
     bin1 = h1->GetBinContent(i);
     bin2 = h1->GetBinContent(i);
@@ -246,7 +166,6 @@ int compTH1I(TDirectoryFile* f1, TDirectoryFile* f2, TKey* key){
       difCtr++;
     }
   }
-  
   
   // user output 
   if (difTot){
@@ -260,17 +179,13 @@ int compTH1I(TDirectoryFile* f1, TDirectoryFile* f2, TKey* key){
 }
   
 
-
 int compTH2F(TDirectoryFile* f1, TDirectoryFile* f2, TKey* key){
-  TH2F * h1 = (TH2F*)f1;//->Get(key->GetName());
-  TH2F * h2 = (TH2F*)f2;//->Get(key->GetName());
-
-  int xTotBins = h1->GetNbinsX();
-  int yTotBins = h1->GetNbinsY();
-
+  TH2F * h1 = (TH2F*)f1;
+  TH2F * h2 = (TH2F*)f2;
   double bin1=0, bin2=0, binDif=0, difTot=0;
   unsigned int difCtr=0;
-
+  int xTotBins = h1->GetNbinsX();
+  int yTotBins = h1->GetNbinsY();
 
   for (int i=0; i<xTotBins; i++ ){
     for (int j=0; j<yTotBins; j++ ){
@@ -281,7 +196,6 @@ int compTH2F(TDirectoryFile* f1, TDirectoryFile* f2, TKey* key){
 	difTot += abs(binDif);     	
 	difCtr++;
       }
-      //cout << " binDif = " << binDif << " ";
     }
   }
   
@@ -300,11 +214,9 @@ int compTH2F(TDirectoryFile* f1, TDirectoryFile* f2, TKey* key){
 int compTH2I(TDirectoryFile* f1, TDirectoryFile* f2, TKey* key){
   TH2I * h1 = (TH2I*)f1;
   TH2I * h2 = (TH2I*)f2;
-
+  int bin1=0, bin2=0, binDif=0, difCtr=0, difTot=0;
   int xTotBins = h1->GetNbinsX();
   int yTotBins = h1->GetNbinsY();
-
-  int bin1=0, bin2=0, binDif=0, difCtr=0, difTot=0;
   
   for (int i=0; i<xTotBins; i++ ){
     for (int j=0; j<yTotBins; j++ ){
@@ -315,7 +227,6 @@ int compTH2I(TDirectoryFile* f1, TDirectoryFile* f2, TKey* key){
 	difTot += abs(binDif);     	
 	difCtr++;
       }
-      //cout << " binDif = " << binDif << " ";
     }
   }
  
@@ -331,29 +242,27 @@ int compTH2I(TDirectoryFile* f1, TDirectoryFile* f2, TKey* key){
 }
 
 
-
 int compTProfile(TDirectoryFile* f1, TDirectoryFile* f2, TKey* key){
   TProfile * p1 = (TProfile*)f1;
   TProfile * p2 = (TProfile*)f2;
 
-TH1D * temp1 = p1->ProjectionX("temp1");
-TH1D * temp2 = p2->ProjectionX("temp2");
-TH1D * h3 = new TH1D(*temp1);
+  TH1D * temp1 = p1->ProjectionX("temp1");
+  TH1D * temp2 = p2->ProjectionX("temp2");
+  TH1D * h3 = new TH1D(*temp1);
 
-h3->Add(temp1,temp2,-1,1); 
+  h3->Add(temp1,temp2,-1,1); 
 
- delete temp1;
- delete temp2;
-
+  delete temp1;
+  delete temp2;
+  
 
   if (h3->GetEntries() != 0.0){
-    //printf("MISMATCH (Tprf): object: %s has h1NumEntries + h2NumEntries =  %f\n", key->GetName(), temp1->GetEntries());
-    // printf("          check: >%s< should be the same as >%s<\n", f1->GetName(), f2->GetName());
+    
     delete h3;
     cout << "FAIL!" << endl;
-  return 1;  
+    return 1;  
   }
-  //SUCCESS;
+ 
   delete h3;
   cout << "PASS!" << endl;
    return 0;
